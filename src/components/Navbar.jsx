1211e6765521bf43097820c2e25991874d84cb5d
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2025-05-20 14:17:38 +0700

    update nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center mx-[144px] h-[100px] sticky">
+    <nav className="flex justify-between items-center mx-[144px] h-[100px] sticky top-0 z-50 bg-white">
       <img src="/logo.svg" alt="" className="w-[180px] h-[60px]" />
       <div className="flex gap-[8px] bg-[#F9FAFB] border-[#E5E7EB] rounded-xl max-w-[250px] h-[40px] pl-[10px]">
         <img src="/search.svg" alt="" className="w-[16px] text-[#6A7282]" />
